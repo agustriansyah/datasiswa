8464dc13d048d9fceac2571e7a04413b41b0344a
agustriansyah <agustricip@gmail.com>
2024-09-05 10:42:23 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/**/*.{html,js}'],
+  theme: {
+    container: {
+        center: true,
+        padding: '16px',
+      },
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+        pacifico: ['Pacifico', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
+
