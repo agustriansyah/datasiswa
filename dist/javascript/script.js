7da052b74d19231c4167aec39ba8c115f3d756f5
agustriansyah <agustricip@gmail.com>
2024-10-08 08:55:16 +0800

    ubah

--- dist/javascript/script.js
+++ dist/javascript/script.js
@@ -9,5 +9,15 @@ function dropDown() {
     document.querySelector('.section').classList.toggle('md:pl-[300px]')
     document.querySelector('.sidebar').classList.toggle('shadow-[2px_0_5px_rgba(0,0,0,0.3)]')
   }
+
+  function confirmDelete() {
+    const result = confirm("Are you sure you want to delete this item?");
+    if (result) {
+      // Logic untuk delete di sini
+      alert("Item deleted successfully!");
+    } else {
+      alert("Item not deleted.");
+    }
+  }
  
   
